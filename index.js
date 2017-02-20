f494559b4bd7878ad59c6ef75d74aeb3f635b4c5
Oracy <oramartos_21@hotmail.com>
2017-02-20 12:35:20 -0300

    aprendizado-personalizado-suissa

---
+++ index.js
@@ -0,0 +1,6 @@
+const Project = require('./model');
+
+const project1 = new Project();
+
+console.log(project1);
+//{ _id: 58aab3e9bf4343703783715d }
---
+++ model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+const Schema = mongoose.Schema;
+
+const __projectSchema = new Schema({
+	nomeProjeto: String,
+	pontuacao: Number,
+	leiDoBem: Number,
+	conteudo: String,
+});
+
+const Project = mongoose.model('Project', __projectSchema);
+
+module.exports = Project;
