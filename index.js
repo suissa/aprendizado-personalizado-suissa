e07e56af3bb68c5917da13bd0b70e88fd6426181
Jean Carlo Nascimento <jnascimento@gmail.com>
2017-02-20 23:58:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const Create = require('./controller.js')
+const Controller = require('./controller.js')
+
+const projectData = {
+  // coloque os dados aqui
+}
+
+const success = ( data ) => console.log('sucesso: ', data)
+const error = ( err ) => console.log('error: ', err)
+
+Controller.create( projectData ).then( success ).catch( error )
